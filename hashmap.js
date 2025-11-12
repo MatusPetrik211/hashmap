2b54a24fb51391e1162ba5fc4d369c34b8e69e01
MatusPetrik211 <172436057+MatusPetrik211@users.noreply.github.com>
2025-11-12 22:33:25 +0100

    Partially repair set method (not finished)

--- hashmap.js
+++ hashmap.js
@@ -4,7 +4,7 @@ function HashMap(loadFactor = 0.75, capacity = 16) {
   return {
     loadFactor,
     capacity,
-    buckets: [],
+    buckets: new Array(capacity),
     hash(key) {
       let hashCode = 0;
       const primeNumber = 31;
@@ -13,19 +13,29 @@ function HashMap(loadFactor = 0.75, capacity = 16) {
         hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % capacity;
       }
       
+      console.log(hashCode)
       return hashCode
     },
     set(key, value) {
-      const keys = this.buckets.map(pair => pair[0]);
+      const index = this.hash(key);
 
-      console.log(keys);
-
-      for (let i = 0; i < this.buckets.length; i++) {
-        console.log("hello");
+      if (index < 0 || index >= this.buckets.length) {
+        throw new Error("Trying to access index out of bounds");
       }
+
+      this.buckets[index] = [key, value]
+
+      console.log(this.buckets)
     }, 
   };
 }
 
 let hashmap = HashMap();
-hashmap.set("key", 3);
+hashmap.set("fdsfdsf", 0);
+hashmap.set("bus", 1);
+hashmap.set("olej", 2);
+hashmap.set("new", 3);
+hashmap.set("old", 4);
+hashmap.set("loop", 5);
+hashmap.set("for", 6);
+hashmap.set("bool", 7);
